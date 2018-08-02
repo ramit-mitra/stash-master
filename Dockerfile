a528e0e60b70784af5f7a3b57d14601b74506174
Ramit Mitra <ramit.mitra@gmail.com>
2018-08-02 13:01:31 +0530

    intermediate code

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,20 @@
 # License : MIT
 # About : Containerised CI/CD with a dedicated GIT server, user management and jenkins.
 FROM node:8.11-alpine
+RUN apk add tar gzip wget sudo git paxctl --no-cache && \
+    mkdir -p /opt/java && \
+    cd /opt/java && \
+    wget http://download.oracle.com/otn-pub/java/jdk/10.0.2+13/19aef61b38124481863b1413dce1855f/jre-10.0.2_linux-x64_bin.tar.gz && \
+    sudo tar -zxvf jre-10.0.2_linux-x64_bin.tar.gz && \
+    ls -la && \
+    ln -s /opt/java/jdk1.8.0_121 /opt/java/current && \
+    touch /etc/profile.d/java.sh && \
+    export JAVA_HOME=/opt/java/current && \
+    export PATH=$PATH:$JAVA_HOME/bin && \
+    /etc/profile.d/java.sh && \
+    ls -la
 WORKDIR /usr/src/stash-master
 COPY . .
 RUN npm install
-EXPOSE 2323 5055 8080
+# EXPOSE 2323 5055 8080
 CMD npm run launch
--- services/stash.js
+++ services/stash.js
@@ -9,11 +9,57 @@ const server = require('node-git-server');
 const repos = new server(global.stashDir, {
     autoCreate: true,
     authenticate: (type, repo, user, next) => {
+
+        console.log(1);
+
+        //push to server operation
         if (type == 'push') {
-            user((username, password) => {
-                console.log(username, password);
-                next();
+            return new Promise((resolve, reject) => {
+
+                console.log(2);
+
+                user((username, password) => {
+                    //check if permission data exists
+                    if (!global.permissions.hasOwnProperty(repo)) {
+
+                        console.log(3);
+
+                        //if there is no existing entry for this repo in permission DS, add a blank key
+                        //and save to file
+                        global.permissions[repo] = [];
+                        fs.writeFile('./app-data/permissions.json', JSON.stringify(global.permissions));
+                        //display an error to user to update permissions
+                        return reject("You donot have permission to access this repository. Please update the permissions for this repository and try again later.");
+                    } else {
+
+                        console.log(4);
+
+                        //validate if user has write permission
+                        for (i in global.permissions[repo]) {
+                            if (global.permissions[repo][i].username == username && global.permissions[repo][i].writeP) {
+
+                                console.log(5);
+
+                                //user has write permission
+                                //validate user password
+                                for (j in global.users.active) {
+                                    if (global.users.active[j].username == username && global.users.active[j].password == password) {
+                                        console.log(6);
+                                        //user authorised
+                                        return resolve();
+                                    }
+                                }
+                            }
+                        }
+                        //user doesn't have permission or invalid password
+                        //display an error to user to update permissions
+                        return reject("Invalid credentials, or missing permission, please consult your Stash Admin.");
+                    }
+                });
             });
+        } else if (type == 'fetch') {
+            //clone & pull operations
+            next();
         } else {
             next();
         }
@@ -22,11 +68,13 @@ const repos = new server(global.stashDir, {
 /* EVENT HANDLERS */
 repos.on('push', (push) => {
     console.log(`push ${push.repo}/${push.commit} (${push.branch})`);
+    console.log(push)
     push.accept();
 });
 
 repos.on('fetch', (fetch) => {
     console.log(`fetch ${fetch.commit}`);
+    console.log(fetch)
     fetch.accept();
 });
 
