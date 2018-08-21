84a3f6a8717aac35d870e67ba8c97363ef8c1a49
Ramit Mitra <ramit.mitra@gmail.com>
2018-08-21 13:39:37 +0530

    App boot validator bug fix

--- validator/validator.js
+++ validator/validator.js
@@ -1,22 +1,23 @@
 /* LOADING DEPENDENCIES */
-const shell = require('shelljs');
-const fs = require('fs');
-
-/**
+var shell = require('shelljs');
+const fs = require('fs');/**
  *  Validate if stash directory is present, else create it
  */
 if (!fs.existsSync(global.stashDir)) {
-  shell.exec('mkdir ' + global.stashDir);
+  shell.mkdir(global.stashDir);
 }
 /**
  *  Validate if app-data files are present, else create it
  */
-if (!fs.existsSync('../app-data/users.json')) {
-  shell.exec('echo "{}" > ../app-data/users.json');
+ if (!fs.existsSync('./app-data')) {
+  shell.mkdir('./app-data');
 }
-if (!fs.existsSync('../app-data/permissions.json')) {
-  shell.exec('echo "{}" > ../app-data/permissions.json');
+if (!fs.existsSync('./app-data/users.json')) {
+  shell.exec('echo "{}" > ./app-data/users.json');
 }
-if (!fs.existsSync('../app-data/pr.json')) {
-  shell.exec('echo "{}" > ../app-data/pr.json');
+if (!fs.existsSync('./app-data/permissions.json')) {
+  shell.exec('echo "{}" > ./app-data/permissions.json');
+}
+if (!fs.existsSync('./app-data/pr.json')) {
+  shell.exec('echo "{}" > ./app-data/pr.json');
 }
